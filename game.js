12096a37c2ff4c8227983892e26be60cfe031fcd
joe <loseca@yahoo.com>
2016-04-17 13:06:59 -0600

    9

--- game.js
+++ game.js
@@ -1,43 +1,81 @@
 
 
 
-var health = 100;
+var health = 100
+  var obj= function (name, mod, description){
+    this.name = name;
+    this.mod = mod;
+    this.description = description;
+  }
+var items = {
+  shield: new obj("Shield", 4, "This is an awesome shield!"),
+  vest: new obj('Vest', 3 , "This is a great vest"),
+  helmet: new obj('Helmet', 2, "This is a sweet helmet"),
+  missed: new obj('Missed',1, "Ha Ha you missed")
+}
+  
+
+
 function slap() {
-  health--;
+ var modifier= player.weapons()
+  health=health-1+ modifier;
   healthBar();
   update();
-} 
+}
 
-function kick(){
-  health--; 
- healthBar();
- update();
-} 
-
-function punch(){
-  health--;
+function kick() {
+  var modifier= player.weapons()
+  health=health-4+ modifier;
   healthBar();
-update();
+  update();
 }
 
-function update(){
-  if( health <= 30){
-         document.getElementById("player-panel").classList.add("panel-danger")
-         alert ('Level Low')
-    }
-    else
-    {
-         document.getElementById("player-panel").classList.remove("panel-danger")
-    }
+function punch() {
+ var modifier= player.weapons() 
+  health=health-2+ modifier;
+  healthBar();
+  update();
+}
+
+function update() {
+  if (health <= 30) {
+    document.getElementById("player-panel").classList.add("panel-danger")
+    alert('Level Low')
+  }
+  else {
+    document.getElementById("player-panel").classList.remove("panel-danger")
+  }
 
 }
+  
+  var player = {
+    puller:[],
+    weapons:function(){
+      var ranMod= Math.floor(Math.random()*this.puller.length)
+      document.getElementById("comment").innerText=this.puller[ranMod].description
+      return this.puller[ranMod].mod
+    
+  }  
+    
+    
+  }
+  for(var item in items){player.puller.push (items[item])
+
 var healthBarElem = document.getElementById('healthbar')
-
-function healthBar()
-{
-healthBarElem.style.width = health + '%'
-healthBarElem.innerText = health
+function healthBar(){ 
+  
+  healthBarElem.style.width = health + '%'
+  healthBarElem.innerText = health
 }
+  
+  }  
+  
+
+   
+
+
+
+
 
 
 
